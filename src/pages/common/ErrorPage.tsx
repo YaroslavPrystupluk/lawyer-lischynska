09b9052140ed19702afa14281816b933e899fe76
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-09-17 17:17:51 +0300

    delete icon 404

--- src/pages/common/ErrorPage.tsx
+++ src/pages/common/ErrorPage.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { useNavigate, useRouteError } from "react-router-dom";
-import { TbError404 } from "react-icons/tb";
 
 import imageErorr from "../../assets/image/404-pages.png";
 
@@ -16,7 +15,6 @@ const ErrorPage: FC = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center h-[100vh] bg-secondary">
       <img className="w-1/2" src={imageErorr} alt="image page not found" />
-      {/* <TbError404 className="text-8xl text-red-400" /> */}
       <h3 className="text-3xl font-bold my-5">Oops! Page Not Found</h3>
       <p className="text-lg mb-5">
         The page you are looking for might have been removed, had its name
