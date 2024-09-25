88d1a5360bd86b81f94df6a074ec6e73d8c59845
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-09-25 17:09:16 +0300

    add

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,7 +3,7 @@ import commonRouter from "./common.router";
 import Layout from "../Layout/Layout";
 import { ROLE } from "../types/types";
 import adminRouter from "./admin.router";
-import { ErrorPage } from "../pages/common";
+import { ErrorPag } from "../pages/common";
 
 const getRouterByRole = (role: ROLE) => {
   switch (role) {
