ad77e1a21133c72eeef5d87c1d45c5e769493c6f
Iaroslav05 <tyucha_05@ukr.net>
2024-09-28 12:49:50 +0300

    change color primary

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,17 +5,17 @@ import lawyer from "../../assets/image/lawyer.webp";
 
 const Header: FC = () => {
   return (
-    <header className="bg-gradient-to-r from-secondary to-secondary/30">
+    <header className="bg-gradient-to-r from-secondary/60 to-secondary">
       <Navbar />
       <div className="grid grid-flow-row sm:grid-flow-col">
         <div className="flex flex-col items-center sm:pl-12 justify-center flex-shrink-">
           <p className="relative text-4xl sm:text-5xl lg:text-6xl font-bold text-primary uppercase">
             <span className="before:content-['\201c'] before:absolute before:top-[-15px] sm:before:top-[-25px] before:left-[-30px] before:text-primary before:text-4xl sm:before:text-5xl lg:before:text-6xl">
-              Репутація
+              Репутація,
             </span>
           </p>
           <p className="text-4xl sm:text-5xl lg:text-6xl font-bold text-primary uppercase">
-            повага
+            повага,
           </p>
           <p className="relative text-4xl sm:text-5xl lg:text-6xl font-bold text-primary uppercase">
             <span className="after:content-['\201d'] after:absolute after:bottom-[-35px] sm:after:bottom-[-50px] after:right-[-30px] after:text-primary after:text-4xl sm:after:text-5xl lg:after:text-6xl">
