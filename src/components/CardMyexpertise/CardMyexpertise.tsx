57fb08a882bbe4f4a979ca6be492f03a7fdc1989
Iaroslav05 <tyucha_05@ukr.net>
2025-05-17 17:37:15 +0300

    change logo

--- src/components/CardMyexpertise/CardMyexpertise.tsx
+++ src/components/CardMyexpertise/CardMyexpertise.tsx
@@ -2,15 +2,14 @@ import { FC } from "react";
 
 import { SvgIcon } from "../SvgIcon/SvgIcon";
 import { CARD } from "../../constants/constants";
+import TitleChapter from "../TitleChapter/TitleChapter";
 
 // import icons from "/icons/icons.svg";
 const CardMyexpertise: FC = () => {
   return (
     <>
       <div className="relative my-8 sm:my-16">
-        <h1 className="text-3xl sm:text-5xl text-center font-bold before:content-[''] before:bg-primary before:absolute before:bottom-[-30%] sm:before:bottom-[-50%] before:left-[50%] before:translate-x-[-50%] before:w-20 before:h-1">
-          Сфери моєї компетенції
-        </h1>
+        <TitleChapter> Сфери моєї компетенції</TitleChapter>
       </div>
       {
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
