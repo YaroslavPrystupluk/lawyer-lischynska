ebd02de899ab3a10a47cd68f129f304c1f1ce152
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2025-07-08 12:20:31 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,11 @@ import lawyer from '/image/lawyer.webp';
 
 const Header: FC = () => {
 	return (
-		<header className="bg-gradient-to-r from-secondary/60 to-secondary">
+		<header className="bg-gradient-to-r from-secondary/60 to-secondary lg:h-[100vh]">
 			{/*<header className="bg-[url('/image/back-header.png')] bg-cover bg-no-repeat h-[500px] md:h-[700px] lg:h-[100vh] ">*/}
 			<Navbar/>
-			<div className="grid grid-flow-row sm:grid-flow-col">
-				
-				<div className="flex flex-col md:flex-row items-center sm:px-12 justify-center">
-					<section className="text-primary uppercase text-xl sm:text-2xl md:text-4xl xl:text-6xl font-bold">
-						{/*<div className="container w-1/2">*/}
+				<div className="flex flex-col md:flex-row items-center pl-12 md:px-12 justify-center">
+					<section className="text-primary uppercase text-xl sm:text-2xl md:text-4xl xl:text-6xl font-bold grow basis-1/3" >
 							<blockquote className="relative px-8 py-8 ">
 								<div
 									className="text-xl sm:text-2xl md:text-4xl xl:text-6xl font-bold text-left">&ldquo;</div>
@@ -22,15 +19,13 @@ const Header: FC = () => {
 								<div
 									className="text-xl sm:text-2xl md:text-4xl xl:text-6xl font-bold text-right">&rdquo;</div>
 							</blockquote>
-						{/*</div>*/}
 					</section>
-					<div
-						className="h-[470px] max-w-[400px] sm:h-[600px] sm:max-w-[500px] lg:h-[700px] lg:max-w-[600px] overflow-hidden justify-self-center">
-						<img className="max-w-full object-cover" src={lawyer} alt="lawyer photo"/>
-					</div>
+					
 				</div>
-			</div>
-		
+				<div
+					className="h-[470px] max-w-[400px] sm:h-[600px] sm:max-w-[500px] lg:h-[700px] lg:max-w-[600px] overflow-hidden justify-self-center grow-0 basis-2/3">
+					<img className="max-w-full" src={lawyer} alt="lawyer photo"/>
+				</div>
 		</header>
 	);
 };
