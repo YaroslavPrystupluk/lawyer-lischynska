1d2a5a5469b8d3ae2fbac5037d0efd14454ab7e9
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2025-08-06 19:25:16 +0300

    add expertise

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,6 @@ import lawyer from "/image/lawyer.png";
 const Header: FC = () => {
   return (
     <header className="bg-gradient-to-r from-secondary/60 to-secondary h-[100vh] overflow-hidden">
-      {/*<header className="bg-[url('/image/back-header.png')] bg-cover bg-no-repeat h-[500px] md:h-[700px] lg:h-[100vh] ">*/}
       <Navbar />
       <div className="flex flex-col md:flex-row items-center md:items-start md:px-12 justify-center">
         <section className="text-primary uppercase text-xl md:text-4xl xl:text-6xl font-bold justify-self-start py-3 md:py-16">
