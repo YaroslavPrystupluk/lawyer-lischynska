007e8846b416132fca659632c82b1bd4b32ce1d4
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2025-08-08 16:26:19 +0300

    fix

--- src/pages/common/Home.tsx
+++ src/pages/common/Home.tsx
@@ -1,14 +1,14 @@
-import {FC} from 'react';
-import CardMyExpertise from '../../components/CardMyExpertise/CardMyExpertise.tsx';
-import StatisticsCount from '../../components/StatisticsCount/StatisticsCount.tsx';
+import { FC } from "react";
+import CardMyExpertise from "../../components/CardMyExpertise/CardMyExpertise.tsx";
+// import StatisticsCount from "../../components/StatisticsCount/StatisticsCount.tsx";
 
 const Home: FC = () => {
-	return (
-		<>
-			<CardMyExpertise/>
-			<StatisticsCount/>
-		</>
-	);
+  return (
+    <>
+      <CardMyExpertise />
+      {/* <StatisticsCount/> */}
+    </>
+  );
 };
 
 export default Home;
