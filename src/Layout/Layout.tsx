401c0bf5653ed419394445c4f2b182211a6a329d
Iaroslav05 <tyucha_05@ukr.net>
2025-08-10 17:41:04 +0300

    add SEO

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import {FC, useRef, useState} from 'react';
+import {FC, useEffect, useRef, useState} from 'react';
 import { Outlet } from "react-router-dom";
 import { Header } from "../components";
 import Footer from "../components/Footer/Footer";
@@ -20,6 +20,12 @@ const Layout: FC = () => {
         }
         setModalIsOpen(false)
     }
+
+    useEffect(() => {
+        if (typeof document !== 'undefined') {
+            document.dispatchEvent(new Event('render-event'));
+        }
+    }, []);
 
   return (
     <>
