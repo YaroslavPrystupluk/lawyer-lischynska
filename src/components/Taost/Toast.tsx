3f584f56b8503087046e21b893516063d618e1c0
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2025-08-11 15:33:31 +0300

    add toast

---
+++ src/components/Taost/Toast.tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+
+interface IToastProps {
+  type: string;
+  message: string;
+  iconName: string;
+}
+
+const Toast: FC<IToastProps> = ({ type, message, iconName }) => {
+  return (
+    <div
+      id="toast-top-left"
+      className="flex items-center w-full max-w-xs p-4 mb-4 text-slate-500 bg-white rounded-lg shadow-sm shadow-slate-700"
+      role="alert"
+    >
+      <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-cyan-800 bg-cyan-200 rounded-lg">
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+        </svg>
+        <span className="sr-only">{iconName}</span>
+      </div>
+      <div className="ms-3 text-sm font-normal">{message}</div>
+      <button
+        type="button"
+        className="ms-auto -mx-1.5 -my-1.5 bg-white text-slate-400 hover:text-slate-900 rounded-lg focus:ring-2 focus:ring-slate-300 p-1.5 hover:bg-slate-100 inline-flex items-center justify-center h-8 w-8"
+        data-dismiss-target={`#toast-${type}`} //TODO
+        aria-label="Close"
+      >
+        <span className="sr-only">Close</span>
+        <svg
+          className="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Toast;
