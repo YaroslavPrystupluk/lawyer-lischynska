59ca0c125842319b6ff0c50785ffa3b233c05256
Iaroslav05 <tyucha_05@ukr.net>
2025-11-06 19:02:30 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,20 @@ import {createRoot} from "react-dom/client";
 import {HelmetProvider} from "react-helmet-async";
 import AppRouter from "./routes/AppRouter";
 import JsonLd from "./components/SEO/JsonLd.tsx";
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query"
 
 import "./firebase/firebaseConfig";
 import "./index.css";
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
+        <QueryClientProvider client={queryClient}>
         <HelmetProvider>
             <JsonLd/>
             <AppRouter/>
         </HelmetProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
